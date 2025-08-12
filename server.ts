d58d9488e117b548108fe30e6bf385b3a3c6acc6
GabrielCae <bcaetan.ofc@gmail.com>
2025-08-12 15:40:03 -0300

    Rota de Deleção e Modificação de curso adicionadas, função de procurar curso adicionada para evitar repetição

--- server.ts
+++ server.ts
@@ -14,14 +14,18 @@ const server = fastify({
     }
 })
 
-const coursers = [
+const courses = [
     { id: '1', title: "Curso de Node.js" },
     { id: '2', title: "Curso de React" },
     { id: '3', title: "Curso de React Native" }
 ]
 
+const findCourse = (id: string) => {
+    return courses.find(course => course.id === id)
+}
+
 server.get('/courses', () => {
-    return { coursers }
+    return { courses }
 })
 
 server.get("/courses/:id", (request, reply) => {
@@ -32,12 +36,12 @@ server.get("/courses/:id", (request, reply) => {
     const params = request.params as Params
     const courseID = params.id
 
-    const course = coursers.find(course => course.id === courseID)
+    const course = findCourse(courseID)
 
     if (course)
         return { course }
     else
-        return reply.status(404)
+        return reply.status(404).send({ message: "Curso não encontrado!" })
 })
 
 server.post('/courses', (request, reply) => {
@@ -52,12 +56,53 @@ server.post('/courses', (request, reply) => {
     if (!courseTitle)
         return reply.status(400).send({ message: "Título obrigatório" })
 
-    coursers.push({ id: courseID, title: courseTitle })
+    courses.push({ id: courseID, title: courseTitle })
 
     // Sempre retornar um objeto
-    return reply.status(201).send({ courseID })
+    return reply.status(201).send({ message: `Curso criado com sucesso! ID: ${courseID}` })
+})
+
+server.delete("/courses/:id", (request, reply) => {
+    type Params = {
+        id: string
+    }
+
+    const params = request.params as Params
+    const courseID = params.id
+
+    const course = findCourse(courseID)
+
+    if (course) {
+        courses.splice(courses.indexOf(course), 1)
+        return reply.status(200).send({ message: "Curso deletado com sucesso!" })
+    } else 
+        return reply.status(404).send({ message: "Curso não encontrado!" })
+})
+
+server.put("/courses/:id", (request, reply) => {
+    type Params = {
+        id: string
+    }
+    
+    type Body = {
+        title: string
+    }
+
+    const params = request.params as Params
+    const courseID = params.id
+
+    const body = request.body as Body
+    const newTitle = body.title
+
+    const course = findCourse(courseID)
+
+    if (course) {
+        course.title = newTitle
+        return reply.status(200).send({ message: "Nome do curso modificado com sucesso!" })
+    } else
+        return reply.status(404).send({ message: "Curso não encontrado!" })
 })
 
 server.listen({ port: 3333 }).then(() => {
-    console.log("HTTP Server runing")
+    console.log("HTTP Server running")
 })
