a9f2578c773912f38f84d85ed9a2d695d3c16a89
GabrielCae <bcaetan.ofc@gmail.com>
2025-08-13 21:19:21 -0300

    Adição de paginação e filtros

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, uuid, text } from "drizzle-orm/pg-core"
+import { pgTable, uuid, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
     id: uuid().primaryKey().defaultRandom(),
@@ -9,5 +9,14 @@ export const users = pgTable("users", {
 export const courses = pgTable("courses", {
     id: uuid().primaryKey().defaultRandom(),
     title: text().notNull().unique(),
-    description: text()    
+    description: text()
 })
+
+export const enrollments = pgTable("enrollments", {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid().notNull().references(() => users.id),
+    courseId: uuid().notNull().references(() => courses.id),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+}, table => [
+    uniqueIndex().on(table.userId, table.courseId)
+])
